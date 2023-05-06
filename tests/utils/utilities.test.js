8117f50323e9d857597cfde1d9f9afa18ed8b20d
NahuelSavedra <nsavedra97@gmail.com>
2023-05-06 16:36:12 -0300

    Test utilities

---
+++ tests/utils/utilities.test.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const { generateHash, comparePasswordWithHash } = require('../.././src/utils/utilities');
+
+jest.mock('bcrypt', () => ({
+    genSalt: jest.fn(() => Promise.resolve('mockedSalt')),
+    hash: jest.fn((password, salt) => Promise.resolve(`mockedHash(${password},${salt})`)),
+    compare: jest.fn((password, passwordHash) => Promise.resolve(password === passwordHash))
+}));
+
+describe('generateHash', () => {
+    it('should call bcrypt.genSalt() and bcrypt.hash() with the given password', async () => {
+        const password = 'password';
+        const expectedSalt = 'mockedSalt';
+        const expectedHash = 'mockedHash(password,mockedSalt)';
+        const passwordHash = await generateHash(password);
+        expect(bcrypt.genSalt).toHaveBeenCalledWith(10);
+        expect(bcrypt.hash).toHaveBeenCalledWith(password, expectedSalt);
+        expect(passwordHash).toEqual(expectedHash);
+    });
+});
+
+describe('comparePasswordWithHash', () => {
+    it('should call bcrypt.compare() with the given password and passwordHash', async () => {
+        const password = 'password';
+        const passwordHash = 'password';
+        const validPassword = await comparePasswordWithHash(password, passwordHash);
+        expect(bcrypt.compare).toHaveBeenCalledWith(password, passwordHash);
+        expect(validPassword).toEqual(true);
+    });
+});
