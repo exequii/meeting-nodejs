88ee90d8353e76490e2636cf3cc1302095659331
Ezequiel <ezequiel.sanson@hotmail.com>
2023-05-06 12:08:35 -0300

    Servicio y Conexion BDD

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,74 @@
 const User = require('../models/user');
 
 const createUser = async (userData) => {
-
+  try {
+    const user = new User(userData);
+    await user.save();
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const getUserByCredentials = async(email) => {
-
+  try {
+      const user = await User.findOne({email});
+      if(!user) return null;
+      return user;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const getUserByFilters = async(body) => {
-
+  try {
+      const users = await User.find(body);
+      if(!users || users.length == 0) return null;
+      return users;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 
 const getAllUsers = async () => {
-
+  try {
+    const users = await User.find();
+    if (!users || users.length == 0) return null;
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
   const getUserById = async (id) => {
-
+    try {
+      const user = await User.findById(id);
+      if (!user) return null;
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 
 const updateUserById = async (id, newData) => {
-
+  try {
+    const userUpdated = await User.findByIdAndUpdate(id,newData, { new: true });
+    if(!userUpdated) return null;
+    return userUpdated;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const deleteUserById = async (id) => {
-
+  try {
+    const user = await User.deleteOne({ _id: id });
+    if(!user) return null;
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 module.exports = { createUser, getAllUsers, getUserById, updateUserById, deleteUserById, getUserByCredentials, getUserByFilters };
