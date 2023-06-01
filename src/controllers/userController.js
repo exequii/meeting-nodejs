f4857c956182923436f1bd51544c67622531ea74
NahuelSavedra <nsavedra97@gmail.com>
2023-06-01 18:52:54 -0300

    Retorno user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,7 +50,7 @@ const getUserByCredentials = async (req, res) => {
     const validPassword = await comparePasswordWithHash(req.body.password, user.password);
     if (!validPassword) return res.status(400).json({ message: 'Password not valid' })
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
-    res.status(200).json({'token' : token});
+    res.status(200).json({'token' : token, 'user': user});
   }catch(error){
     //console.error(error)
     res.status(500).json({ message: 'Internal Server Error', error: error.message });
