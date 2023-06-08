72b01a10523cbdecf38a485111a2e094cb90c0e8
Ezequiel <ezequiel.sanson@hotmail.com>
2023-06-07 21:50:46 -0300

    Saco middleware donde no va

--- src/delivery/router/routes/common.routes.js
+++ src/delivery/router/routes/common.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../../middlewares/authMiddleware');
 const {getTechnologies} = require('../../controllers/commonController');
 
-router.get('/technologies', authMiddleware, getTechnologies);
+router.get('/technologies', getTechnologies);
 
 module.exports = router;
