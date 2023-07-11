95673d31a46ef9f3fc5f17c0d4722d6cb3f63001
Ezequiel <ezequiel.sanson@hotmail.com>
2023-07-10 21:24:20 -0300

    Prueba2

--- src/infrastructure/utils/pdf.js
+++ src/infrastructure/utils/pdf.js
@@ -3,7 +3,10 @@ const path = require('path');
 
 async function generatePDF(html) {
     return new Promise((resolve, reject) => {
-        const options = { format: 'Letter' };
+        const options = { 
+            format: 'Letter',
+            phantomPath: "./node_modules/phantomjs/bin/phantomjs"
+        };
         //pdf.create(html, options).toFile('Certificate_1.pdf',(err, buffer) => {
         pdf.create(html, options).toBuffer((err, buffer) => {
             if (err) {
