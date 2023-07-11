601eb655e5182275fc68d22fed56a41f4bbb63b0
Ezequiel <ezequiel.sanson@hotmail.com>
2023-07-10 21:44:15 -0300

    Revert "Cambios"
    This reverts commit 365ceb9f6898484a26fdbd4619ccadd09e4b61ec.

--- src/infrastructure/utils/pdf.js
+++ src/infrastructure/utils/pdf.js
@@ -5,6 +5,7 @@ async function generatePDF(html) {
     return new Promise((resolve, reject) => {
         const options = { 
             format: 'Letter',
+            phantomPath: "./node_modules/phantomjs/bin/phantomjs"
         };
         //pdf.create(html, options).toFile('Certificate_1.pdf',(err, buffer) => {
         pdf.create(html, options).toBuffer((err, buffer) => {
